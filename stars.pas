45142d70663f3cb5f1c5220a85a21038cc5193d4
Marques Johansson <marques@displague.com>
2014-08-28 23:45:44 -0400

    reindented with "ptop -i 4 -l 120 stars.pas ..."

--- stars.pas
+++ stars.pas
@@ -1,102 +1,117 @@
 {Program Scape}
 {$linklib gcc}
 {$linklib SDLmain}
+
 Uses dos,
-     sdl,
-     crt;
+sdl,
+crt;
 {$asmmode intel}
 
-label 1, 2, 3, 4, 5, 6, l1, l2, l3;
-CONST MaxStars = 400; ScreenDist = 40;
-Type S1 = Record
-     X,Y: Integer;
-     Z: Byte;
-End;
-Type S2 = Record
-     X,Y: Integer;
+Label 1, 2, 3, 4, 5, 6, l1, l2, l3;
+
+Const MaxStars =   400;
+    ScreenDist =   40;
+
+Type S1 =   Record
+    X,Y:   Integer;
+    Z:   Byte;
 End;
 
-Var Star: Array[1..MaxStars] of S1;
-Var OldStars: Array[1..MaxStars] of S2;
-Var Stars: Array[1..MaxStars] of S2;
-Var X, Y, T, C: Integer;
-Var scr: PSDL_Surface;
+Type S2 =   Record
+    X,Y:   Integer;
+End;
+
+Var Star:   Array[1..MaxStars] Of S1;
+
+Var OldStars:   Array[1..MaxStars] Of S2;
+
+Var Stars:   Array[1..MaxStars] Of S2;
+
+Var X, Y, T, C:   Integer;
+
+Var scr:   PSDL_Surface;
 
 Procedure PutPixel (Xx, Yy : Integer; Col: Byte);
-begin
-scr[Xx + Yy * 320]:=Col;
-end;
+Begin
+    scr[Xx + Yy * 320] := Col;
+End;
 
 Procedure NewStar;
-label l1, l2, l3;
+
+Label l1, l2, l3;
 Begin;
-l1: Star[T].Z:= 63;
- IF Star[T].Z = 0 then goto l1;
-l2: Star[T].X:= Random(320) - 160;
- IF Star[T].X = 0 then goto l2;
-l3: Star[T].Y:= Random(200) - 100;
- IF Star[T].Y = 0 then goto l3;
+    l1:   Star[T].Z := 63;
+    If Star[T].Z = 0 Then goto l1;
+    l2:   Star[T].X := Random(320) - 160;
+    If Star[T].X = 0 Then goto l2;
+    l3:   Star[T].Y := Random(200) - 100;
+    If Star[T].Y = 0 Then goto l3;
 End;
 
 Begin;
-Randomize;
+    Randomize;
 
-if SDL_Init( SDL_INIT_VIDEO ) < 0 then HALT;
-scr:=SDL_SetVideoMode(320,200,24, SDL_SWSURFACE);
+    If SDL_Init( SDL_INIT_VIDEO ) < 0 Then HALT;
+    scr := SDL_SetVideoMode(320,200,8, SDL_SWSURFACE);
 
 
-For T:= 1 to MaxStars do BEGIN
-1: Star[T].Z:= Random(63) + 1;
- IF Star[T].Z = 0 then goto 1;
-2: Star[T].X:= Random(320) - 160;
- IF Star[T].X = 0 then goto 2;
-3: Star[T].Y:= Random(200) - 100;
- IF Star[T].Y = 0 then goto 3;
-END;
+    For T:= 1 To MaxStars Do
+        Begin
+            1:   Star[T].Z := Random(63) + 1;
+            If Star[T].Z = 0 Then goto 1;
+            2:   Star[T].X := Random(320) - 160;
+            If Star[T].X = 0 Then goto 2;
+            3:   Star[T].Y := Random(200) - 100;
+            If Star[T].Y = 0 Then goto 3;
+        End;
 
-Repeat
+    Repeat
 
-For T:= 1 to MaxStars do BEGIN
- Stars[T].X:= ScreenDist * Star[T].X div Star[T].Z;
- Stars[T].Y:= ScreenDist * Star[T].Y div Star[T].Z;
-End;
+        For T:= 1 To MaxStars Do
+            Begin
+                Stars[T].X := ScreenDist * Star[T].X Div Star[T].Z;
+                Stars[T].Y := ScreenDist * Star[T].Y Div Star[T].Z;
+            End;
 
-For T:= 1 to MaxStars do BEGIN
- PutPixel(OldStars[T].X, OldStars[T].Y, 0);
-End;
+        For T:= 1 To MaxStars Do
+            Begin
+                PutPixel(OldStars[T].X, OldStars[T].Y, 0);
+            End;
 
-For T:= 1 to MaxStars do BEGIN
- X:= Stars[T].X + 160;
- Y:= Stars[T].Y + 100;
- IF (X > 0) and (X < 320) and (Y > 0) and (Y < 200) Then PutPixel(X,Y, Star[T].Z + 192);
-END;
+        For T:= 1 To MaxStars Do
+            Begin
+                X := Stars[T].X + 160;
+                Y := Stars[T].Y + 100;
+                If (X > 0) And (X < 320) And (Y > 0) And (Y < 200) Then PutPixel(X,Y, Star[T].Z + 192);
+            End;
 
 {WaitRetrace;}
 
-For T:= 1 to MaxStars do BEGIN
- OldStars[T].X:= Stars[T].X + 160;
- OldStars[T].Y:= Stars[T].Y + 100;
- Star[T].Z:= Star[T].Z - 1;
- X:= Stars[T].X + 160;
- Y:= Stars[T].Y + 100;
- IF (X < 0) or (X > 320) or (Y < 0) or (Y > 200) then NewStar;
- IF Star[T].Z = 0 then NewStar;
-End;
+        For T:= 1 To MaxStars Do
+            Begin
+                OldStars[T].X := Stars[T].X + 160;
+                OldStars[T].Y := Stars[T].Y + 100;
+                Star[T].Z := Star[T].Z - 1;
+                X := Stars[T].X + 160;
+                Y := Stars[T].Y + 100;
+                If (X < 0) Or (X > 320) Or (Y < 0) Or (Y > 200) Then NewStar;
+                If Star[T].Z = 0 Then NewStar;
+            End;
 
-Until KeyPressed;
+    Until KeyPressed;
 
-SDL_Quit;
+    SDL_Quit;
 
-Textcolor(15);
-Textbackground(0);
-Writeln('                 Hope you liked this Star Field Simulation');
-Writeln;
-Writeln('       If your version was modified and you would like to have a copy');
-Writeln('      of the original, please Email me, please specify BASIC or PASCAL');
-Writeln('                           Email:  pwac@flinet.com');
-Writeln('        If you would like any programs writin or if you live in the');
-Writeln('               West Palm Beach Florida area. ACS can help you.');
-Writeln('   For any of the above mentioned please visit http://www.flinet.com/~pwac');
-WriteLn;
+    Textcolor(15);
+    Textbackground(0);
+    Writeln('                 Hope you liked this Star Field Simulation');
+    Writeln;
+    Writeln('       If your version was modified and you would like to have a copy');
+    Writeln('      of the original, please Email me, please specify BASIC or PASCAL');
+    Writeln('                           Email:  pwac@flinet.com');
+    Writeln('        If you would like any programs writin or if you live in the');
+    Writeln('               West Palm Beach Florida area. ACS can help you.');
+    Writeln('   For any of the above mentioned please visit http://www.flinet.com/~pwac');
+    WriteLn;
 
-END.
+End.
